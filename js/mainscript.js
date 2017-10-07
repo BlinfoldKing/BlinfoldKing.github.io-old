d47958b7c7a2b084badf5f00995485722602674d
BlinfoldKing <ganeshadanu1@gmail.com>
2017-10-07 23:07:31 +0700

    ALMOST DONE

--- js/mainscript.js
+++ js/mainscript.js
@@ -19,9 +19,32 @@ $("#Btn1").click(function(){
 })
 
 $("#term1").click(function(){
+  $('#win2').slideDown(300);
+})
 
-  $('#win2').show(300);
+$("#term2").click(function(){
+  $('#win3').slideDown(300);
+})
 
+$("#term3").click(function(){
+  $('#win4').slideDown(300);
+})
+
+$("#exit1").click(function(){
+  $(".exit").parent().slideUp(300);
+  $(".G").fadeIn(300);
+})
+
+$("#exit2").click(function(){
+  $("#win2").slideUp(300);
+})
+
+$("#exit3").click(function(){
+  $("#win3").slideUp(300);
+})
+
+$("#exit4").click(function(){
+  $("#win4").slideUp(300);
 })
 
 $(function() {
