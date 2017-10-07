083a4bfb3941800571d4d9c5a363a2f99e2f8e42
BlinfoldKing <ganeshadanu1@gmail.com>
2017-10-07 15:37:58 +0700

    update

--- js/mainscript.js
+++ js/mainscript.js
@@ -12,12 +12,24 @@ $(".G").hover(function(){
 $('.terminal').hide();
 $('.G').click(function(){
   $('.terminal').slideToggle();
-
   toggleable = !toggleable;
   termisopen = !termisopen;
   if(termisopen){
-    $('.header').html("ganesha.exe");
+    $('.header').html("ganesha.exe <div class='exit'>x</div>");
+
   }else {
-      $('.header').html("Click to continue")
+      $('.header').html("Click to continue <div class='exit'>x</div>");
+  }
+})
+
+$('.exit').click(function(){
+  $('.terminal').slideToggle();
+  toggleable = !toggleable;
+  termisopen = false;
+  if(termisopen){
+    $('.header').html("ganesha.exe <div class='exit'>x</div>");
+
+  }else {
+      $('.header').html("Click to continue <div class='exit'>x</div>");
   }
 })
