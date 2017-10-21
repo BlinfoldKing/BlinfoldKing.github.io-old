159db9c38ce7bde3f8f612684e4e3a0c90efca68
BlinfoldKing <ganeshadanu1@gmail.com>
2017-10-21 13:53:05 +0700

    bug fixed

--- js/mainscript.js
+++ js/mainscript.js
@@ -31,28 +31,28 @@ $("#exit1").click(function(){
   $(".exit").parent().slideUp(300);
   $(".G").fadeIn(300);
 })
-()
+
 $("#exit2").click(function(){
   $("#win2").slideUp(300);
-})
+});
 
 $("#exit3").click(function(){
   $("#win3").slideUp(300);
-})
+});
 
 $("#exit4").click(function(){
   $("#win4").slideUp(300);
-})
+});
 
 
 $("#exit32").click(function(){
   $("#win32").slideUp(300);
-})
+});
 
 $("#exit22").click(function(){
   $("#win22").slideUp(300);
-})
+});
 
 $(function() {
     $( ".draggable" ).draggable();
-  });
+});
